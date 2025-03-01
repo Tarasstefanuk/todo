e3fc48ea342f008ad4c0fb9fd141efe3668bf1e8
Taras Stefanuk <tarasstefanuk920@gmail.com>
2025-03-01 23:28:51 +0200

    sss

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+
 export default defineConfig({
   base: "/todo/", 
   plugins: [react()],
 })
+
+
+
+
+
+
